2b6ac5d701df581994ab78e5dbb3b58ddca93051
devxiy <115905665+devxiy@users.noreply.github.com>
2026-01-06 11:24:43 -0500

    feat: Agregar código de seguimiento de Hotjar para analytics de usuarios

--- app/layout.js
+++ app/layout.js
@@ -22,16 +22,35 @@ export default function RootLayout({ children }) {
         content="VEy9xJOy75Ui3-w29BTFPp7jKeXcvtkgxEa3IzSfgp0"
       />
       {isProduction && (
-        <Script
-          id="tag-manager-head"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <>
+          <Script
+            id="tag-manager-head"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-T8V9MML2');`,
-          }}
-        />
+            }}
+          />
+          {/* Hotjar Tracking Code for Jetour */}
+          <Script
+            id="hotjar-tracking"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:6614022,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `,
+            }}
+          />
+        </>
       )}
       <body className={`${poppins.variable} antialiased`}>{children}</body>
     </html>
